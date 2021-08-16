e4dae362a99aa6ced8ac5d6d317feb36268686ff
Holy-Mortal <holy_mortal@163.com>
2021-08-16 08:39:03 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 import { createApp } from 'vue'
-import Vant from 'vant'
-import 'vant/lib/index.css'
-import 'amfe-flexible'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
+// 加载全局样式
 import './styles/index.less'
 
+// 加载动态设置 REM 基准值
+import 'amfe-flexible'
+
 const app = createApp(App)
-// app.config.globalProperties.$http = () => {
-//   console.log('http请求')
-// }
+
+// 注册使用 Vant 组件库
 app.use(Vant)
+
 app.use(store).use(router).mount('#app')
